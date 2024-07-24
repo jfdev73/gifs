b4ea9eeb0f7fc9d637b30363f1bba38c6fa6053b
Fernando Miranda <fermirandamonroy@hotmail.com>
2024-07-24 17:36:56 -0600

    se creo servicio, la interfaz y se muestra ya los gifs en pantalla

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,9 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) {}
+
+  get tagsHistory() {
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string) {
+    this.gifsService.searchTag(tag);
+  }
+}
